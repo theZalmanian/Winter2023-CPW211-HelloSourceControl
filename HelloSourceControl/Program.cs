054fd123bc50001efad285ce63d23892f5f6832e
Zalman Izak <thezalmanian@gmail.com>
2023-01-08 18:23:09 -0800

    - Added user input fields to customize output

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -4,9 +4,15 @@
     {
         static void Main(string[] args)
         {
-            for(int i = 1; i < 6; i++)
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("How many greetings would you like? ");
+            int numGreetings = Convert.ToInt32(Console.ReadLine());
+
+            for (int currIndex = 0; currIndex < numGreetings; currIndex++)
             {
-                Console.WriteLine($"({i}) Hello, GitHub!");
+                Console.WriteLine($"{currIndex + 1}) Hello, {name}!");
             }
         }
     }
