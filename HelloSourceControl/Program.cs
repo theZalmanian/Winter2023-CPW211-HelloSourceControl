deed154486dcfdd638563092f07e302738320f6c
Zalman Izak <thezalmanian@gmail.com>
2023-01-08 18:27:36 -0800

    - Added documentation

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -4,15 +4,18 @@
     {
         static void Main(string[] args)
         {
+            // Get user's name
             Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+            string userName = Console.ReadLine();
 
+            // Get number of requested greetings
             Console.Write("How many greetings would you like? ");
             int numGreetings = Convert.ToInt32(Console.ReadLine());
 
+            // Display greeting for given # of times
             for (int currIndex = 0; currIndex < numGreetings; currIndex++)
             {
-                Console.WriteLine($"{currIndex + 1}) Hello, {name}!");
+                Console.WriteLine($"{currIndex + 1}) Hello, {userName}!");
             }
         }
     }
